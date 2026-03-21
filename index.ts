1a84f69577f832457c80d824d77dd90704a2d263
Luftjunkie19 <luftjunkie@protonmail.com>
2026-03-21 19:09:24 +0100

    play-around with socket.io to understand mechanism

--- index.ts
+++ index.ts
@@ -1,25 +1,55 @@
 import express from 'express';
-
 import { Server } from 'socket.io';
-
-import {createServer} from "https";
+import { createServer } from 'http'; // Use HTTP instead of HTTPS for local dev
 
 const app = express();
-
 const server = createServer(app);
 
 const socketio = new Server(server, {
-    cors:{origin:['http://localhost:3000']}
+  cors: { origin: ['http://localhost:3000'], methods:["POST", "GET", "DELETE", "UPDATE"] },
+  connectionStateRecovery: {
+    'maxDisconnectionDuration': 1000
+  }
 });
 
-socketio.on('connection', (socket)=>{
-    console.log(socket.data);
-    socket.on("hello", (args)=>{
-        console.log(args);
-    });
-})
+socketio.on('connection', (socket) => {
+  const walletAddress = socket.handshake.auth.walletAddress;
+  
+  const isAuthenticated = walletAddress && 
+                          walletAddress.startsWith("0x") && 
+                          walletAddress.length === 42;
 
-server.listen(2137, ()=>{
-    console.log("tuto bene !");
-})
 
+  socket.on('join-room',(roomId)=>{
+   if (!isAuthenticated) {
+    socket.emit('no_auth', { msg: "No authentication" });
+    // socket.disconnect(true); // Disconnect immediately
+    return;
+  }
+    socket.join(roomId);
+    socket.emit("join-response", {message:`You have joined ${roomId}-room`});
+  });
+
+  socket.on('send-to-room', (roomId, message)=>{
+    socketio.to(roomId).emit('message_to_room', message);
+  });
+
+  socket.on("leave-room", (roomId)=>{
+    if(socket.rooms.has(roomId)){
+    socket.leave(roomId);
+    socket.emit("leave-room-msg", {message:'Bye hope to see you again :D'});
+  }else{
+socket.emit('not_member', {msg:`You are not the member of the room ${roomId}`});
+  }
+
+  });
+    
+  socket.on('disconnect', () => {
+    console.log('Client disconnected:', socket.id);
+  });
+
+});
+
+server.listen(2137, () => {
+  console.log('Server running on port 2137');
+});
